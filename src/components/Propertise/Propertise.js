73610e21ad19965697a129ba336ae8acab34f589
saifsammy <saifsammy653@gmail.com>
2022-09-29 21:32:58 +0600

    trying adding toast

--- src/components/Propertise/Propertise.js
+++ src/components/Propertise/Propertise.js
@@ -1,6 +1,7 @@
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Propertise.css'
+import './Propertise.css';
+
 import React from 'react';
 
 const Propertise = ({ time }) => {
