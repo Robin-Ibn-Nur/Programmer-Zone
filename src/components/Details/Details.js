bef77d0ca95e0a8f9db8e517575c2af2d1437039
saifsammy <saifsammy653@gmail.com>
2022-09-30 23:28:40 +0600

    final touch

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './Details.css'
 
 const Details = ({ item, timeAddToProfile }) => {
-    const { name, time, img, text } = item;
+    const { name, time, img, text, ratings } = item;
 
     return (
         <div className='details-card'>
             <img src={img} alt="" />
-            <h3>Course Name:{name}</h3>
-            <small>About Us: {text}</small>
-            <p>Duration:{time}<sub>s</sub> </p>
+            <div>
+                <h4>Course Name:{name}</h4>
+                <small>About Us: {text}</small>
+                <p>Duration:{time}<sub>s</sub> </p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
             <button onClick={() => timeAddToProfile(item)}>Add to List</button>
         </div>
     );
